d0519729d509acd27397f18297119f5c9bade863
nimmis <kjell.havneskold@gmail.com>
2016-08-10 09:57:35 +0200

    remove unused js code

--- root/etc/apache2/internal/error.js
+++ root/etc/apache2/internal/error.js
@@ -1,11 +1,5 @@
     window.onload = function() {                                                          
-       var root=document.getElementById('foo') ;                                          
        root.innerHTML = document.location; // location.protocol + '//' + location.host; //
-//       setTimeout(function(){                                                             
-//           document.body.style.backgroundImage="url('"+bg_pic+"')";                 
-//           document.body.style.backgroundRepeat = "no-repeat";;                                  
-//       },1000);                                                                                   
-//
        setTimeout(function(){document.getElementById('pic').style.visibility = "visible";},800);
        setTimeout(function(){document.getElementById('foo').style.visibility = "visible";},2000); 
        setTimeout(function(){document.getElementById('foo2').style.visibility = "visible";},2000);
