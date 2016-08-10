10c521965a4fd1842b2c17ad7458220b768462f7
nimmis <kjell.havneskold@gmail.com>
2016-08-10 12:31:49 +0200

    clean up naming on files and tags

--- root/etc/apache2/internal/error.js
+++
@@ -1,6 +0,0 @@
-    window.onload = function() {                                                          
-       root.innerHTML = document.location; // location.protocol + '//' + location.host; //
-       setTimeout(function(){document.getElementById('pic').style.visibility = "visible";},800);
-       setTimeout(function(){document.getElementById('foo').style.visibility = "visible";},2000); 
-       setTimeout(function(){document.getElementById('foo2').style.visibility = "visible";},2000);
-    }
